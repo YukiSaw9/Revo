89d6413cf0f7dcf440614754880226b93149b86b
Yuki Saw <90437632+YukiSaw9@users.noreply.github.com>
2021-09-19 00:06:50 +0630

    Add files via upload

---
+++ Revo.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Donate via
+
+# ## >> YouTube
+# ### Description:
+# #### - Stream with VPN (Europe region, US region,etc..)
+# #### - Watch till the end without pausing
+# #### - Watch ads without skipping...
+
+# [![my image](https://img.youtube.com/vi/jBfGpgNchRA/0.jpg)](https://www.youtube.com/watch?v=jBfGpgNchRA)
+
+# [![my image](http://img.youtube.com/vi/W6SJ0bJLWB0/0.jpg)](https://www.youtube.com/watch?v=W6SJ0bJLWB0)
+
+# ## >> Spring Zodiac
+
+# * [Go to link 1](https://ouo.io/3HxUti)
+# * [Go to link 2](https://ouo.io/Wpjf9s)
+# * [Go to link 3](https://ouo.io/Wpjf9s)
+
+# ## >> Click to donate
+
+# * [Go to link1](https://ouo.io/TY9AZB)
+# * [Go to link2](https://ouo.io/OUlxH2)
+# * [Go to link3](https://ouo.io/sWNu8EA)
