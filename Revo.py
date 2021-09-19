ec6df2237ad46c5bc185a2c9453f8b45e817e85b
Yuki Saw <90437632+YukiSaw9@users.noreply.github.com>
2021-09-19 20:26:06 +0630

    Add files via upload

---
+++ Revo.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Donate Via (No money, No worries, Just click)
+# ## This site allows you to donate for revolution without the need for a single penny. Just click, stream and Have FUN!!!
+
+# ## >> YouTube
+# ### Description:
+# #### - Stream with VPN (Europe region, US region,etc..)
+# #### - Watch till the end without pausing
+# #### - Watch ads without skipping...
+
+# [![my image](https://img.youtube.com/vi/jBfGpgNchRA/0.jpg)](https://www.youtube.com/watch?v=jBfGpgNchRA)
+
+# [![my image](http://img.youtube.com/vi/W6SJ0bJLWB0/0.jpg)](https://www.youtube.com/watch?v=W6SJ0bJLWB0)
+
+# ## >> Spring Zodiac
+
+# * [Go to link 1](https://ouo.io/3HxUti)
+# * [Go to link 2](https://ouo.io/Wpjf9s)
+# * [Go to link 3](https://ouo.io/Wpjf9s)
+
+# ## >> Click to donate
+
+# * [Go to link1](https://ouo.io/TY9AZB)
+# * [Go to link2](https://ouo.io/OUlxH2)
+# * [Go to link3](https://ouo.io/sWNu8EA)
+
+# ### Thank you for visiting our site... 
+# ### Come and visit our site tomorrow...
+
+# > WE MUST WIN. III
